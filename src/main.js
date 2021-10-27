f1b170c06d32669601633670b23b6e9a0b678b54
RJA7 <kopanskyy.roman@gmail.com>
2021-10-27 13:53:05 +0300

    Add versions list page

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const versionLocalStorageKey = 'version';
+
+const versions = [
+  '1.0.x',
+];
+
+const root = document.getElementById('root');
+
+function main() {
+  const savedVersion = localStorage.getItem(versionLocalStorageKey);
+
+  if (versions.includes(savedVersion)) {
+    showLastUsedVersion(savedVersion);
+  } else {
+    showVersionsList();
+  }
+}
+
+function switchToVersion(version) {
+  window.location.href = `./${version}`;
+}
+
+function handleVersionClick(version) {
+  localStorage.setItem(versionLocalStorageKey, version);
+  switchToVersion(version);
+}
+
+function showVersionsList(lastUsedVersion) {
+  root.innerHTML = `
+<div class="versions-list">
+  <div style="font-size: 24px; line-height: 40px;">Choose a version</div>
+  ${lastUsedVersion ? (`<div class="version-item" onclick="handleVersionClick('${lastUsedVersion}');">${lastUsedVersion} last used</div>`) : ''}
+  ${versions.map((version) => `<div class="version-item" onclick="handleVersionClick('${version}');">${version}</div>`).join('')}
+</div>`;
+}
+
+function showLastUsedVersion(version) {
+  root.innerHTML = `
+<div class="versions-list">
+  <div style="font-size: 24px; line-height: 40px;">Last used version</div>
+  <div class="version-item" onclick="handleVersionClick('${version}');">${version}</div>
+  <div class="version-item" onclick="showVersionsList('${version}')">Show other versions</div>
+</div>`;
+}
+
+main();
